4ba985cd3ecbbc777ec9c5fd134ae211e6c7b1b1
RustamHuseynov02 <147059960+RustamHuseynov02@users.noreply.github.com>
2024-03-19 18:10:43 +0400

    Create rest-bookshopping file

---
+++ projects/rest-bookshopping/src/main/java/com/example/rustem/restbookshopping/RestBookshoppingApplication.java
@@ -0,0 +1,13 @@
+package com.example.rustem.restbookshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBookshoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestBookshoppingApplication.class, args);
+	}
+
+}
---
+++ projects/rest-bookshopping/src/test/java/com/example/rustem/restbookshopping/RestBookshoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rustem.restbookshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestBookshoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
