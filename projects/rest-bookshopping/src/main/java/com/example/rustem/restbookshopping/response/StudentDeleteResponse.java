c23ddb88021bc7f1c4ec9d2626ab87a0767f10af
RustamHuseynov02 <rustem.huseynov.2016@gmail.com>
2024-03-30 02:18:12 +0400

    Create StudentDeleteResponse.java

---
+++ projects/rest-bookshopping/src/main/java/com/example/rustem/restbookshopping/response/StudentDeleteResponse.java
@@ -0,0 +1,12 @@
+package com.example.rustem.restbookshopping.response;
+
+import lombok.Data;
+
+@Data
+public class StudentDeleteResponse {
+
+	private Integer id;
+	private String name;
+	private String surname;
+	private String username;
+}
