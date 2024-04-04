e343da6d1840746d0abc952e82c75dffa7bb0801
RustamHuseynov02 <rustem.huseynov.2016@gmail.com>
2024-04-04 16:34:40 +0400

    Update UserRepository.java

--- projects/rest-bookshopping/src/main/java/com/example/rustem/restbookshopping/repository/UserRepository.java
+++ projects/rest-bookshopping/src/main/java/com/example/rustem/restbookshopping/repository/UserRepository.java
@@ -1,11 +1,28 @@
 package com.example.rustem.restbookshopping.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.rustem.restbookshopping.entity.User;
 
-public interface UserRepository extends JpaRepository<User, String> {
+import jakarta.transaction.Transactional;
+
+@Transactional
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 	User username(String a);
+
+//	@Query(value = "UPDATE users SET username=?1 where username=?2", nativeQuery = true)
+//	@Modifying
+//	void saveByUser(String newUsername, String username);
+
+	@Query(value = "delete from users where username=?1", nativeQuery = true)
+	@Modifying
+	void deleteByUsername(String username);
+
+	Optional<User> findByUsername(String username);
 
 }
